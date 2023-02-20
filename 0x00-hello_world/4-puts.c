732edf6c458376018b684fe788c1ac9df21aecce
Ubaisrael <ubachukwudiisrael@gmail.com>
2023-02-20 09:11:30 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multiligual
+ * puzzle,followed by a newline."
+ * return 0.
  */
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multiligual puzzle");
+
+	return(0);
 }
